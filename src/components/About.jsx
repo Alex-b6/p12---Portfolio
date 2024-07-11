819cd89add19e99d11d339de5b1e373c87bcd842
AlexB6 <brou.alexandre.pro@gmail.com>
2024-07-11 09:45:23 +0200

    Add : Added better responsive for mobile.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -24,30 +24,25 @@ const About = () => {
         <div className="section-headers">
           <div className="section-header" onClick={() => toggleSection('skills')}>
             <h3>Compétences</h3>
+            <div className={`content ${visibleSections.skills ? 'visible' : ''}`}>
+              <ul>
+                <li>Développement front-end avec HTML, CSS et JavaScript.</li>
+                <li>Conception et intégration d'interfaces utilisateur réactives.</li>
+                <li>Gestion de projets web et collaboration en équipe.</li>
+              </ul>
+            </div>
           </div>
           <div className="section-header" onClick={() => toggleSection('education')}>
             <h3>Formation</h3>
+            <div className={`content ${visibleSections.education ? 'visible' : ''}`}>
+              <p>J'ai suivi une formation de développeur web - intégrateur avec Openclassrooms, où j'ai acquis des compétences approfondies en HTML, CSS, JavaScript et développement web.</p>
+            </div>
           </div>
           <div className="section-header" onClick={() => toggleSection('goals')}>
             <h3>Objectifs professionnels</h3>
-          </div>
-        </div>
-
-        <div className="content-wrapper">
-          <div className={`content ${visibleSections.skills ? 'visible' : ''}`}>
-            <ul>
-              <li>Développement front-end avec HTML, CSS et JavaScript.</li>
-              <li>Conception et intégration d'interfaces utilisateur réactives.</li>
-              <li>Gestion de projets web et collaboration en équipe.</li>
-            </ul>
-          </div>
-
-          <div className={`content ${visibleSections.education ? 'visible' : ''}`}>
-            <p>J'ai suivi une formation de développeur web - intégrateur avec Openclassrooms, où j'ai acquis des compétences approfondies en HTML, CSS, JavaScript et développement web.</p>
-          </div>
-
-          <div className={`content ${visibleSections.goals ? 'visible' : ''}`}>
-            <p>Je suis passionné par l'apprentissage continu et je cherche à approfondir mes compétences en développement et à contribuer à des projets innovants.</p>
+            <div className={`content ${visibleSections.goals ? 'visible' : ''}`}>
+              <p>Je suis passionné par l'apprentissage continu et je cherche à approfondir mes compétences en développement et à contribuer à des projets innovants.</p>
+            </div>
           </div>
         </div>
       </div>
